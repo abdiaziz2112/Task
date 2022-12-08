d0398c489b1d1bc2f00af55ee15f8c56f4170012
abdiaziz2112 <maashaaabdiaziz@gmail.com>
2022-12-08 11:34:38 +0300

    version 4

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,37 @@
 document.addEventListener("DOMContentLoaded", () =>{
+
+    // by default , submit button disabled
+    document.querySelector("#submit").disabled = true;
+
+    document.querySelector("#task").onkeyup = () => {
+        // If value of input charecter  greaterthan zero make btn disabled
+        if (document.querySelector("#task").value.length > 0) {
+
+            document.querySelector("#submit").disabled = false;
+            
+        }
+        // If value of input charecter  greaterthan zero make btn enabled or active
+        else{
+            document.querySelector("#submit").disabled = true;
+        }
+        
+    }
+
+    // find form container from dom or html file and do the fallowing task
     document.querySelector("form").onsubmit = () =>{
+
+        // find input or txtfeild   task from dom or html file 
         const task = document.querySelector("#task").value;
-        const li = document.createElement('li');
+
+         // craete new element of list
+        const li = document.createElement("li");
         li.innerHTML = task;
-        document.querySelector('#tasks').append(li);
-        document.querySelector("#task").value = ";
+          // add new item to task list
+        document.querySelector('#tasks').append(li)
+           // clear task input after submitting
+        document.querySelector("#task").value = "";
+        // make diabled btn when submiting form
+        document.querySelector("#submit").disabled = true;
     
         
 
